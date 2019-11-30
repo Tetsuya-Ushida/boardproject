6718ebf45a3a6f26a54e6cce709ff06aa88fc23c
Ushida Tetsuya <ushidatetsuya1115@gmail.com>
2019-12-01 00:18:19 +0900

    ログイン機能作成

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,11 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from .models import BoardModel
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def signupfunc(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = User.objects.create_user(username, '', password)
-        return render(request, 'signup.html', {'some':100})
+        username2 = request.POST['username']
+        password2 = request.POST['password']
+        try:
+            User.objects.get(username=username2)
+            return render(request, 'signup.html', {'error':'このユーザーは登録されています'})
+        except:
+            user = User.objects.create_user(username2, '', password2)
+            return render(request, 'signup.html', {'some':100})
     return render(request, 'signup.html', {'some':100})
+
+def loginfunc(request):
+    if request.method == 'POST':
+        username2 = request.POST['username']
+        password2 = request.POST['password']
+        user = authenticate(request, username=username2, password=password2)
+        if user is not None:
+            login(request, user)
+            return redirect('list')
+            
+        else:
+            return redirect('login')
+    return render(request, 'login.html')
+
+@login_required
+def listfunc(request):
+    object_list = BoardModel.objects.all()
+    return render(request, 'list.html', {'object_list': object_list})
